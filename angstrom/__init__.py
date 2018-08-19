f256789dc2deb1f726976578a82fb19d7e19e390
Simon <liquidwicked@gmail.com>
2018-08-19 15:43:45 +0200

    fix build and remove some build artifacts from repo

--- angstrom/__init__.py
+++ angstrom/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.0.4"
 
-from angstrom import mobj
-from angstrom import base_connector
-from angstrom import file_system_sql_loader
-from angstrom import Db
+from angstrom.angstrom import mobj
+from angstrom.angstrom import base_connector
+from angstrom.angstrom import file_system_sql_loader
+from angstrom.angstrom import Db
